370e6cdce5ddc74572228216bac12f49bd3796df
Charlie Passfield <charliepassfield@Charlies-MacBook-Air.local>
2023-08-14 18:24:19 +0100

    use TPOT for automl

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import KFold, RandomizedSearchCV, train_test_split, GridSearchCV, cross_val_score
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, mean_absolute_percentage_error
 from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
 from tpot import TPOTRegressor
 
 from modelling.individual_player_models import Modelling
@@ -35,6 +37,19 @@ candidate_columns =['total_points_player',
                     'last_5_team_goals_opp_team',
                     ]
 
+gk_columns =['total_points_player',
+                    'last_5_team_match_points',
+                    'last_5_games_points_player',
+                    'last_5_team_match_points_opp_team',
+#                    'season_points_per_match_opp_team',
+ #                   'transfers_in_player',
+ #                   'transfers_out_player',
+                    'was_home_player',
+ #                   'last_5_team_against',
+                    'season_goals_against_per_match_player_team',
+                    'last_5_team_goals_opp_team',
+                    ]
+
 random_forest_params = {
                 'model__n_estimators': [50, 100, 150],
                 'model__max_features': ['log2', 'sqrt'],
@@ -49,16 +64,16 @@ ridge_params  = {
     }
 
 decision_tree_params = {
-        'splitter':["best", "random"],
-        'max_depth': [3,5,7],
+        'model__splitter':["best", "random"],
+        'model__max_depth': [3,5,7],
     }
 
 kneighbors_parameters = {
-        'n_neighbors':[3,5,7,10]
+        'model__n_neighbors':[1,3,5,7,10]
     }
 
-player_model = Modelling(df, high_scoring_gks, candidate_columns, Ridge(), ridge_params)
-# feature_counts, player_count, player_mae, best_features, best_model = player_model.optimise_feature_selection('player', 5)
+player_model = Modelling(df, high_scoring_gks, candidate_columns, KNeighborsRegressor(), kneighbors_parameters)
+# feature_counts, player_count, player_mae, best_features, best_model = player_model.optimise_feature_selection(5)
 
 # mean_mape = np.mean(list(player_mae.values()))
 
@@ -71,5 +86,5 @@ player_model = Modelling(df, high_scoring_gks, candidate_columns, Ridge(), ridge
 # print(f"The percentage of players used was {player_count/len(high_scoring_gks)}\n")
 # for feature, count in feature_counts.items():
 #     print(f"{feature}: {count}")
-player_df = player_model.position_specific_model_build('Erling Haaland')
-print(player_df)
+#player_model.position_specific_model_build(high_scoring_gks)
+player_model.tpot_modelling(['David de Gea'], gk_columns, 20)
--- src/modelling/individual_player_models.py
+++ src/modelling/individual_player_models.py
@@ -7,6 +7,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import KFold, RandomizedSearchCV, train_test_split, GridSearchCV, cross_val_score
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, mean_absolute_percentage_error
 from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
 from tpot import TPOTRegressor
 
 class Modelling:
@@ -35,7 +37,7 @@ class Modelling:
         columns_dict = {column_name: 0 for column_name in self.feature_candidates}
         return columns_dict
 
-    def player_model_preprocessing(self, df):
+    def player_model_preprocessing(self, df, points_threshold=10, minutes_threshold=15):
         '''
         Prepare the dataframe for a single player so it can be used in a model.
         
@@ -45,12 +47,13 @@ class Modelling:
         Returns:
             pd.DataFrame: Preprocessed DataFrame for a single player.
         '''
+        df = df.copy()
         df = df.dropna()
     
-        # Filter to only have games where players have played more than 15 minutes.
+        # Filter to only have games where players have played more than minutes_threshold minutes.
         # Also filter out hard to predict high scores.
-        df = df[df['minutes_player'] > 15]
-        df = df[df['total_points_player'] <= 10]
+        df = df[df['minutes_player'] > minutes_threshold]
+        df = df[df['total_points_player'] <= points_threshold]
         df = df.loc[:, self.feature_candidates]
         return df
     
@@ -128,25 +131,72 @@ class Modelling:
         return best_feature_subset, best_model, best_mae
     
     def position_specific_model_build(self,
-                                      player,
-                                    #model,
-                                    #model_hyperparameters, 
-                                    num_feature_subsets=20
-                                    ):
+                                      player_list,
+                                      num_feature_subsets=100
+                                      ):
         
         best_mae = float('inf')
         best_feature_subset = None
 
         for _ in range(num_feature_subsets):
-            selected_features = np.random.choice(self.feature_candidates, size=np.random.randint(6, len(self.feature_candidates) + 1), replace=False)
+            print(_)
+            all_features = self.feature_candidates.copy()
+            all_features.remove("total_points_player")
+            selected_features = np.random.choice(all_features, size=np.random.randint(6, len(all_features) + 1), replace=False)
+            mae_list = []
 
-        player_df = self.df[self.df['name_player'] == player].copy()
-        player_df = self.player_model_preprocessing(player_df)
-        
-        return selected_features
-        
+            for player in player_list:
+
+                pipeline = Pipeline(steps= [
+                    ('scaler', StandardScaler()),
+                    ('model', self.model),
+                    ])
+                
+                player_df = self.df[self.df['name_player'] == player].copy()
+                player_df = self.player_model_preprocessing(player_df)
+                X_train, X_test, y_train, y_test = self.train_test_splitting(player_df)
+                X_train_subset, X_test_subset = X_train[selected_features], X_test[selected_features]
+
+                random_search = RandomizedSearchCV( estimator=pipeline,
+                                                    param_distributions=self.model_hyperparameters,
+                                                    n_iter=5,
+                                                    scoring='neg_mean_absolute_error',
+                                                    cv=5,
+                                                    random_state=42,
+                                                    n_jobs=-1,
+                                                    verbose=0
+                                                    )
+                random_search.fit(X_train_subset, y_train)
+                best_model_for_features = random_search.best_estimator_
+                y_pred = best_model_for_features.predict(X_test_subset)
+                test_mae = mean_absolute_error(y_test, y_pred)
+                mae_list.append(test_mae)
+
+            mean_mae = sum(mae_list)/len(mae_list)
+            
+            if mean_mae < best_mae:
+                best_mae = mean_mae
+                best_feature_subset = selected_features
+                print(f"The mae got updated to {best_mae}, using the parameters of \n {best_feature_subset}")
+
+        print(f"The final mae is {best_mae}, using the parameters of \n {best_feature_subset}")
+        return best_feature_subset, best_mae
     
-    def optimise_feature_selection(self, model_type, num_feature_subsets=2):
+    def tpot_modelling(self, players, feature_subset, generations=5):
+        '''Using tpot to find an optimal model with the features that seem most reasonable using domain knowledge.
+        Once an optimal model is found using TPOT, further feature selection could be done on this model.'''
+
+        # Filter df to required players only
+        df = self.df.copy()
+        filtered_df = df[df['name_player'].isin(players)]
+        filtered_df = self.player_model_preprocessing(filtered_df)
+        filtered_df = filtered_df[feature_subset]
+        X_train, X_test, y_train, y_test = self.train_test_splitting(filtered_df)
+        tpot = TPOTRegressor(generations=generations, cv=5, random_state=42, verbosity=2, scoring="neg_mean_absolute_error", n_jobs=-1)
+        tpot.fit(X_train, y_train)
+        print(tpot.score(X_test, y_test))
+
+    def optimise_feature_selection(self, num_feature_subsets=2):
         '''
         Optimize feature selection for multiple players and count the occurrences of selected features.
         
@@ -161,94 +211,25 @@ class Modelling:
         players_used = 0
         player_model_scores = {}
         
-        if model_type == 'player':
-            for player in self.player_list:
-                player_df = self.df[self.df['name_player'] == player].copy()
-                player_df = self.player_model_preprocessing(player_df)
-                X_train, X_test, y_train, y_test = self.train_test_splitting(player_df)
-                player_best_features, player_best_model, player_mae_score = self.player_specific_model_build(X_train,
-                                                                                                            X_test,
-                                                                                                            y_train,
-                                                                                                            y_test,
-                                                                                                            self.model,
-                                                                                                            self.model_hyperparameters,
-                                                                                                            num_feature_subsets
-                                                                                                            )
+        for player in self.player_list:
+            player_df = self.df[self.df['name_player'] == player].copy()
+            player_df = self.player_model_preprocessing(player_df)
+            X_train, X_test, y_train, y_test = self.train_test_splitting(player_df)
+            player_best_features, player_best_model, player_mae_score = self.player_specific_model_build(X_train,
+                                                                                                        X_test,
+                                                                                                        y_train,
+                                                                                                        y_test,
+                                                                                                        self.model,
+                                                                                                        self.model_hyperparameters,
+                                                                                                        num_feature_subsets
+                                                                                          )
 
-                player_model_scores[player] = player_mae_score   
+            player_model_scores[player] = player_mae_score   
 
-                if player_mae_score < 2:
-                    players_used += 1
-                    for value in player_best_features:
-                        if value in feature_dict:
-                            feature_dict[value] += 1
+            if player_mae_score < 2:
+                players_used += 1
+                for value in player_best_features:
+                    if value in feature_dict:
+                        feature_dict[value] += 1
 
         return feature_dict, players_used, player_model_scores, player_best_features, player_best_model
-
-
-# if __name__ == "__main__":
-
-#     df = pd.read_csv("data/processed_data/complete_df.csv")
-#     high_scoring_gks = high_points_by_position(df, 'GK', 120)
-
-#     candidate_columns =['total_points_player',
-#                     'last_5_team_match_points',
-#                     'last_5_team_goals',
-#                     'last_5_player_goals',
-#                     'last_5_games_points_player',
-#                     'last_5_ind_goals_ratio_player',
-#                     'last_5_ind_assists_ratio_player',
-#                     'season_goals_per_match_player_team',
-#                     'last_5_team_match_points_opp_team',
-#                     'last_5_team_against_opp_team',
-#                     'season_goals_against_per_match_opp_team',
-#                     'season_points_per_match_opp_team',
-#                     'transfers_in_player',
-#                     'transfers_out_player',
-#                     'was_home_player',
-#                     'last_5_team_against',
-#                     'season_goals_against_per_match_player_team',
-#                     'last_5_team_goals_opp_team',
-#                     ]
-
-#     random_forest_params = {
-#                 'model__n_estimators': [50, 100, 150],
-#                 'model__max_features': ['log2', 'sqrt'],
-#                 'model__max_depth': [None, 5, 10],
-#                 'model__min_samples_split': [2, 5],
-#                 'model__min_samples_leaf': [1, 2],
-#                 'model__bootstrap': [True, False]
-#             }
-
-#     ridge_params  = {
-#         'model__alpha': [0, 0.0001, 0.001, 0.01, 0.1],
-#     }
-
-#     decision_tree_params = {
-#         'splitter':["best", "random"],
-#         'max_depth': [3,5,7],
-#     }
-
-#     kneighbors_parameters = {
-#         'n_neighbors':[3,5,7,10]
-#     }
-
-#     player_model = Modelling(df, high_scoring_gks, candidate_columns, Ridge(), ridge_params)
-#     # feature_counts, player_count, player_mae, best_features, best_model = player_model.optimise_feature_selection('player', 5)
-
-#     # mean_mape = np.mean(list(player_mae.values()))
-
-#     # print("Player Model MAE Scores:")
-#     # for player, mae in player_mae.items():
-#     #     print(f"{player}: {mae}")
-
-#     # print("\n")
-#     # print(f"Mean mape is {mean_mape}\n")
-#     # print(f"The percentage of players used was {player_count/len(high_scoring_gks)}\n")
-#     # for feature, count in feature_counts.items():
-#     #     print(f"{feature}: {count}")
-#     player_df = player_model.position_specific_model_build('Erling Haaland')
-#     print(player_df)
-
-    
-
