2ead4641de5603eb6663049a2a8b67c60e54e69e
Charlie Passfield <charliepassfield@Charlies-MacBook-Air.local>
2023-09-22 15:50:02 +0100

    clean sheet model

--- src/models/clean_sheet.py
+++ src/models/clean_sheet.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
+from imblearn.over_sampling import SMOTE
 
 # Get the directory containing this script
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -36,6 +37,15 @@ def logistic_regression_pipeline(df):
     # Split the data into train and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=41)
 
+    print(X_train.shape)
+    print(X_train)
+
+    # Oversampling
+    smote = SMOTE(random_state=42)
+    X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
+
+    print(X_train.shape)
+
     # Create a pipeline with scaling and logistic regression
     pipeline = Pipeline([
         ('scaler', StandardScaler()),        # StandardScaler for feature scaling
@@ -43,12 +53,12 @@ def logistic_regression_pipeline(df):
     ])
 
     # Perform cross-validation
-    cv_scores = cross_val_score(pipeline, X_train, y_train, cv=10)  # 5-fold cross-validation
+    cv_scores = cross_val_score(pipeline, X_train_resampled, y_train_resampled, cv=10)  # 5-fold cross-validation
 
     # Fit the pipeline on the entire training data
-    pipeline.fit(X_train, y_train)
+    pipeline.fit(X_train_resampled, y_train_resampled)
 
-    # Evaluate the pipeline on the test data
+    # # Evaluate the pipeline on the test data
     test_score = pipeline.score(X_test, y_test)
     y_pred = pipeline.predict(X_test)
     f1 = f1_score(y_test, y_pred)
@@ -59,37 +69,18 @@ def logistic_regression_pipeline(df):
     print("F1 score:", f1)
     print("Proportion of clean sheets:", y.mean())
 
-def auto_ml_modelling(df, generations=5):
-    '''Using tpot to find an optimal model with the features that seem most reasonable using domain knowledge.
-    Once an optimal model is found using TPOT, further feature selection could be done on this model.'''
-    # Filter df to required players only
-    df = df.copy()
-    X = df.drop('clean_sheets_player', axis=1)
-    y = df['clean_sheets_player']
-
-    scaler = StandardScaler()
-    scaled_X = scaler.fit_transform(X)
-    X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.2, random_state=41, stratify=y)
-
-    tpot = TPOTClassifier(generations=generations, cv=5, random_state=42, verbosity=2, n_jobs=-1)
-    tpot.fit(X_train, y_train)
-
-    print("Proportion of clean sheets:", y.mean())
-    print(tpot.score(X_test, y_test))
-
-
 if __name__ == "__main__":
     gk_columns = [
-        # "last_10_games_points_player",
+         "last_10_games_points_player",
          "player_win_chance", 
-        # "player_lose_chance",
-        # "match_draw_chance",
+         "player_lose_chance",
+         "match_draw_chance",
          "chance_of_over_2.5_goals",
-        # "season_goals_against_per_match_player_team",
+         "season_goals_against_per_match_player_team",
          "clean_sheets_player",
-        # "last_10_team_goals_opp_team",
-        # "last_10_team_against"
+         "last_10_team_goals_opp_team",
+         "last_10_team_against"
         ]
     
     df = prepare_data('GK', 120, gk_columns)
-    auto_ml_modelling(df)
+    logistic_regression_pipeline(df)
